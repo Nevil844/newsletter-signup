1058e288bdcd912c2e1ab5b2a6508dd766471c74
Nevil844 <111491420+Nevil844@users.noreply.github.com>
2023-07-13 16:21:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app=express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server started on port 3000");
+});
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/")
+});
+
+app.post("/", function(req,res){
+
+    const firstName=req.body.fName;
+    const lastName=req.body.lName;
+    const email=req.body.email;
+
+    const data={
+        members:[{
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+      
+    const jsonData = JSON.stringify(data);
+
+    const url= "https://us21.api.mailchimp.com/3.0/lists/e3b3278b5f";
+    const options={
+        method:"POST",
+        auth: "nevil:679859927c3c10425b05450c37ddac08-us21"
+    }
+
+    const request=https.request(url,options, function(response){
+
+        if (response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+}); 
+
+
+
+
+// 679859927c3c10425b05450c37ddac08-us21 
+// e3b3278b5f
